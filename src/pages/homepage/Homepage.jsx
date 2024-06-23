223a4562f8beddf3656c4abd96a350697f5be344
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-06-23 16:04:11 +0600

    todo-list-update-desplay-firebase-realtime-data-interface

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -9,40 +9,31 @@ const Homepage = () => {
     const tododata = useSelector((state) => state.userdata.value)
     const [todoList , setTodoList] = useState([])
     const [parsonalTodoList , setparsonalTodoList] = useState([])
-    let [todo , setTodo] = useState({
-        todo : ""
-    })
+    let [todo , setTodo] = useState('')
     let hendleTodo = (e) => {
-        let {name , value} = e.target
-        setTodo({...todo , [name]:value})
+        setTodo(e.target.value)
     }
 
     useEffect(()=>{
         const userTodoRef = ref( db , "todoLists")
         onValue(userTodoRef , (snapshot) => {
             let array = []
-            snapshot.forEach((item) => {
-                if(tododata.uid == item.key){
-                    array.push({...item.val() , id:item.key})
+            snapshot.forEach((items) => {
+                if(tododata.uid == items.key){
+                    array.push({...items.val() , id:items.key})
                 }
             })
             setTodoList(array)
         })
 
     },[])
-    let handleAdd = (personalTodo)=>{
-        set(ref(db , 'parsonalTodo/' + personalTodo.id) , {
-            todos : todo
-        })
-        alert("todo send Succesful")
-        console.log(personalTodo) 
-    }
+
     useEffect(()=>{
         const userparsonalTodoRef = ref( db , "parsonalTodo")
         onValue(userparsonalTodoRef , (snapshot) => {
             let array = []
             snapshot.forEach((item) => {
-                if(tododata.uid == item.key){
+                if(tododata.uid !== item.key){
                     array.push({...item.val() , id:item.key})
                 }
             })
@@ -50,27 +41,39 @@ const Homepage = () => {
         })
 
     },[])
+    let handleAdd = ()=>{
+        set(push(ref(db , 'parsonalTodo')) , {
+            todos : todo
+        })
+        setTodo("")
+        alert("todo send Succesful")
+    }
     console.log(parsonalTodoList);
+    let handleTodoDelete = (deletetodo) => {
+        remove(ref(db , 'parsonalTodo/' + deletetodo.id)).then(() =>{
+            alert("Delete Todo Succesfuly")
+        })
+    }
   return (
    <section id='home-page'>
         <div className="container">
             <div className='home-page-contant-wrapper'>
                 <Heads text='Enter ypur list' style='home-page-heading'/>
                 <div className="input-box">
-                    <input className='input-list' type='text' placeholder='inter your list' name = "todo" onChange={hendleTodo}/>
+                    <input className='input-list' type='text' placeholder='inter your list' value={todo} name = "todo" onChange={hendleTodo}/>
                 </div>
                {
                 todoList &&
                 todoList.map((item , index) => (
                      <div key={index} className='home-page-add-btn-box'>
-                        <button onClick={() =>handleAdd(item)} className='home-page-btn'>Add</button>
+                        <button onClick={handleAdd} className='home-page-btn'>Add</button>
                     </div>
                 ))
                }
                {
                 parsonalTodoList &&
-                parsonalTodoList.map((item , index) => (
-                    <li key={index}>{item.todo}</li>
+                parsonalTodoList.map((items , index) => (
+                    <li className='persolan-todos' key={index}>{items.todos}<button className='personal-todo-delete' onClick={() =>handleTodoDelete(items)}>Delete</button></li>
                 ))
                }
             </div>
